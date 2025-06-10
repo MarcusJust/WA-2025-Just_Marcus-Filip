9423d136ae80abb62551460f90785ab3ecaee67b
Marcus Just <filip.marcus.just@gmail.com>
2025-06-10 12:48:58 +0200

    #9 cca 4 strany textu, další text bude doplněn

--- final-project/public/about.php
+++ final-project/public/about.php
@@ -42,6 +42,125 @@
 
             <h1 class="mb-4 text-warning">Jak začít?</h1>
             
+            <h3>Urči si cíl</h2>
+
+            <p>V první řadě je dobrá si ujasnit čeho chci dosánout. Chci zhubout? Chci nabrat? Chci vybudovat postavu řeckého boha? Nebo mi stačí jen zlepšit kondici? Podle cíle totiž můžeš lépe zvolit cestu k němu.</p>
+
+            <h3>Uvědom si</h2>
+
+            <p>Ať si zvolil jakýkoliv cíl pamatuj, že jde o to abys se cítil sám lépe. Nenech se tedy odradit či ovlivnit ostatními okolo. Jde o tebe a o to si to užít.</p>
+
+            <p>Je jedno koho potkáš po cestě. Každý někde začínal. Není důvod se cítit špatně kvůli tomu, jak na tom právě jsi. Už jen to že začínáš je obdivuhodné.</p>
+
+            <h3>Neboj se</h2>
+
+            <p>Nemusíš se bát přijít do fitka. Tvoje představa "Co když se mi budou smát nebo mě odsuzovat?", je milná. Ano, najdou se výjimky které takové budou, ale větší část taková opravdu není. Většina je tam kvůli sobě. Je jim tedy jedno jaký jsi ty.</p>
+
+            <p>Nevíš si rady? Tak se prostě ptej. Většina lidí si sice hledí svého, ale zároveň ráda poradí. Opravdu nikdo nechce vidět jak se zraníš. Samozřejmě je dobré přijít a zeptat se slušností a pokorou.</p>
+
+            <h3>Zvaž své možnosti</h2>
+
+            <p>Pokud začínáš sám, tak zvaž své schopnosti. Zvládneš se sám naučit jednotlivé cviky? Na internetu určitě najedeš spoustu videí jak cvik provádět. Je ale potřeba porozumět pohybu. Pokud tuto dovednost ještě nemáš, tak se právě ptej lidí okolo. Případně v začátku vždy stojí za to zvážit možnost osobního trenéra.</p>
+
+            <p>Uvědom si jaký typ člověka jsi a jaké časové možnosti máš. Tohle ti pomůže později.</p>
+
+            <h1 class="text-warning mb-4">První kroky</h1>
+
+            <p>Teď když jsi vše promyslel a uvědomil si, je čas převést to na realitu.</p>
+
+            <ul>
+                <li>Slož si teninkový plán.</li>
+                <li>Mysli na cardio.</li>
+                <li>Nezapomeň na protahování a rozcvičování.</li>
+                <li>Jez kvalitně a vyváženě.</li>
+                <li>Suplementuj jen to co je opravdu potřeba.</li>
+                <li>Dobře spi.</li>
+                <li>Vytvoř si rutinu.</li>
+                <li>Foť se a dokumentuj svou cestu.</li>
+            </ul>
+
+            <h1 class="text-warning mb-4">Jak si složit treninkový plán</h1>
+
+            <p>Právě tady se ti bude hodit uvážení jaký typ člověka jsi a jaké časové možnosti máš. Porozumění oboum aspektům ti pomůže přijít na to jak často si můžeš dovolit chodit. S tím samozřejmě bude souviset ale i cíl.</p>
+
+            <p>U všech možností bude platit, že je dobré se vždy před treninkem zahřát. Ať už lehký cardiem, ale i procházka do fitka se počítá. Po zahřátí se alespoň zlehka protáhnout. Po treninku taky není na škodu protažení. Protažení po treninku omezí ztuhlost druhý den.</p>
+
+            <p>Volbu cviků nechám na tobě. Případně se ptej lidí okolo nebo v naší komunitě. Mysli na to, že většina partií má komplexní strukrutu. Je tedy dobré volit zatím komplexní cviky. Případně mezi treninky cviky obměňovat.</p>
+
+            <h3>Naprostý začátek</h3>
+
+            <p>Pro naprostý začátek bude nejlepší když budeš chodit cvičit jednou až dvakrát týdně. Tělo pravděpodobně ještě není zvyklé na takový styl pohybu. Dej mu čas se adaptovat. Zároveň ti to pomůže pomalu proniknout do tohoto koníčku. V neposlední řadě ti tento přístup pomůže se nepřehltit a neznechutit si to už od začáítku.</p>
+
+            <p>Při této četnosti je nejlepší volbou odcvičit vždy celé tělo. Do toho patří ruce (biceps a triceps), ramena (přední, zadní a boční), záda, hrudník, břicho (spodní, vrchní a boční) a samozřejmě i nohy (quadricepsy, hamstringy a lýtka).</p>
+
+            <p>Obecně se doporučuje každou partii odcvičit dvakrát týdně. Pokud začínáš pouze s jedním treninkem, tak se ale žádné drama neděje. Zároveň je dobré myslet na odpočinek. U treninku celého těla by nebylo dobré jít dva dny za sebou. Platí tedy mít alespoň jeden až dva dny odpočinku mezi treninky.</p>
+
+            <p>Abys ve fitku nestrávil celý den, tak si dej například dva až tři cviky na partii. Je dobré myslet na to že se teprve učíš. Takže spíše vol přístup více opakování s nižší vahou.</p>
+
+            <h3>Mírně pokročilý trenink</h3>
+
+            <p>Už nějakou dobu cvičíš a chceš se posunout? Nebo jsi jen uvážil, že nechceš začít úplně od začátku? Pak si tu rozebereme trochu pokročilejší přístup. Abych se úplně neopakoval i zde platí stejný přístup k zahřátí a protežení.</p>
+
+            <p>Dalším logickým krokem je cvičit třikrát až čtyřikrát do týdne. Pro toto rozložení nejlépe sedí chodit cvičit obden. Pokud ti to časově nevychátí, tak opět není problém. Stačí myslet na jeden až dva dny odpočinku mezi opakovaným cvičením stejné partie.</p>
+
+            <p>Zde už budeme partie lehce rozdělovat. Při tomto rozložení bude jednou z nejlepších možností si treninky rozdělit na vršek (ramena, ruce, hrudník, záda) a spodek (přední stehna, zadní stehna a lýtka). Břicho si zde můžeš rozdělit dle potřeby, či časové náročnosti.</p>
+
+            <p>Už si můžeš i dovolit více cviků na partii. Na příklad jednou můžeš začít hrudníkem a dát důraz na něho. Příště začít zády a dát důraz na ně.</p>
+
+            <h3>Pokročilý trenink</h3>
+
+            <p>Tento přístup už opravdu nebude vhodný pro naprostý začátek. Mimo to je i časově náročný.</p>
+
+            <p>Sem se nejčastějí řadí dva styly treninku. Jsou tak zvaně push-pull-legs a Arnold split. Případně je možná kombinace. Není však vyloučen už naprosto osobní přístup podle potřeby.</p>
+
+            <p>Tato rozdělení nám už dávají opravdu velkou svobodu v množství cviků na partii. Je však dobrá mezi tyto treninky rozdělit důraz na jednotlivé části hlavních partií. Na příklad u hrudníku jednou důraz na spodní  a střední část a podruhé na vrchní část.</p>
+
+            <h4>Push-pull-legs</h4>
+
+            <p>Zde platí rozdělení partií podle typu pohybu. Tedy tlačivý, tahací a nohy. Rozděleníí partií je následovné do push patří hrudník, tricepsy a ramena. Pull zahrnuje záda a bicepsy. Legs jak už z názvu vyplívá zahrnuje nohy. Vychází to tedy na tři treninky. Břicho zde opět můžeme rozdělit dle potřeby.</p>
+
+            <p>Tyto tři treninky jsou vždy tři v řadě jdoucí dny. Následně můžeme dát den odpočinku a začít znovu.</p>
+
+            <h4>Arnold split</h4>
+
+            <p>Zde už dochází k velkému promíchání. Opět je trenink rozdělen do tří po sobě jdoucích dní i přístup k odpočinku je stejný. První den jsou velké partie vršku, což jsou záda a hrudník. Následují menší partie vršku, ramena a ruce. Zakončení jsou opět nohy. Břicho opět volitelně dle potřeby.</p>
+
+            <p>Pokud bys zvolil tento přístup je opravdu nutné mezi jednotlivými cykly měnit důraz. Na příklad záda a hrudník jsou oboje silově náročné partie. Je tedy dobré střídat jejich pořadí mezi cykly.</p>
+
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <a href="views/post/create.php" class="btn btn-outline-warning rounded-pill mt-4">Zeptat se</a>
+            <?php endif; ?>
+
+            <p></p>
+
+            <h1 class="text-warning mb-4">Strava</h1>
+
+            <p>Strava je nedílnou součástí tohoto koníčku ať už je cíl jakýkoliv. Dodává nám živiny pro růst svalové hmoty, energii a často je s ní i spojená psychické pohoda.</p>
+
+            <p>Opravdu ale není nutné okamžitě skočit do nějaké brutální diety. Obecně se říká že rozdělení 80:20 je skvělé. Rozdělení představuje balanc mezi zdravým a nezdravým jídlem. Přeci jen kdo by neměl radost z té chuti nezdravého jídla.</p>
+
+            <p>Při stavě tvoří základ množství kalorií. To se mění podle cíle a s ním případně i z čeho kalorie získáváme. Pro určení množství kalorií můžeme buď kontaktovat profesionáli, nebo s mnohem menší přesností využít internetové kalkulačky. Základ kalorií tvoří náš bazální metabolismus + spálená kalorie za den.</p>
+
+            <p>Podle cíle kalorie přijaté ze stravy zvyšujeme, snižujeme či dorovnáváme vůči koloriím spáleným.</p>
+
+            <p>Pro účely sledování kalorií a živin nám budou dobrým přítelem aplikace jako jsou Kalorické tabulky.</p>
+
+            <p>Pokud si nevíme rady se složením živin či celkového jídelníčku, tak se můžeme otočit na internet případně specialisty.</p>
+
+            <h1 class="text-warning mb-4">Suplementace</h1>
+
+            <p>Suplementovat chceme převážně živiny a vitamíny, které běžnou stravou nedokážeme získat v dostatečném množství. Suplementy však nejsou plnohodnoutnou náhradou jídla.</p>
+
+            <h3>Co suplementovat</h3>
+
+            <p>Pro fitness se velmi rychle stane potřebným suplementování bílkovin. Je nutné myslet ale na dostatečný příjem i z běžné stravy. Tam se vyskutují na příklad v mase, vajíčkách, mléčných výrobcích a některých luštěninách.</p>
+
+            <p>Po stránce vitamínů a minerálů je dobré doplňovat hořčík a vitamín D.</p>
+
+            <p>Pokud nejíme dostatek ryb doplňujeme aké Omega mastné kyseliny.</p>
+
+            <p>Pro fitness jako takové můžeme přidávat na příklad kreatin nebo různé stimulanty pro lepší trenink. U těch je však potřeba dbát na opatrnost.</p>
+
             <a href="index.php" class="btn btn-outline-warning rounded-pill mt-4">Zpět na hlavní stránku</a>
         </div>
     </div>
